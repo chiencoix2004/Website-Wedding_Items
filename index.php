cca3a64722fc40da15d425f9f27a1e0fcc43f7a6
chiencoix2004 <tacongchien003@gmail.com>
2024-01-19 17:38:07 +0700

    Chiến update

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+require_once './common/connect-db.php';
+require_once './common/helper.php';
+
+$act = $_GET['act'] ?? null;
+
+match ($act) {
+    'dssp' => productList(),
+     'ctsp' => productDetail($_GET['id']),
+     default => trangChu(),
+};
+
+
+require_once './common/disconnect-db.php';
